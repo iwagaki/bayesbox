08bdc3999ade36498645f58f7fe685a965c9597f
iwagaki <iwagaki@3d40163a-cb53-0410-a544-1fd9c11f5b11>
2008-08-22 05:25:31 +0000

    renamed GaussianParameter to MixtureParameter

--- trunk/include/bayesbox/MixtureParameter.h
+++ trunk/include/bayesbox/MixtureParameter.h
@@ -1,5 +1,5 @@
-#ifndef GAUSSIAN_PARAMETER_H_
-#define GAUSSIAN_PARAMETER_H_
+#ifndef MIXTURE_PARAMETER_H_
+#define MIXTURE_PARAMETER_H_
 
 #include <cstdio>
 #include <cmath>
@@ -12,8 +12,8 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/matrix_proxy.hpp>
 
-#include "RandomGenerator.h"
-#include "Bayes.h"
+#include "bayesbox/RandomGenerator.h"
+#include "bayesbox/Bayes.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,12 +26,12 @@
 using namespace boost::numeric;
 
 
-class GaussianParameter : public IMCMCParameter
+class MixtureParameter : public IMCMCParameter
 {
 public:
-    GaussianParameter(RandomGenerator& rng) : m_pRng(&rng), m_step(0.0), m_temperature(1.0) {}
+    MixtureParameter() : m_pRng(0), m_temperature(1.0) {}
 
-    ~GaussianParameter() {}
+    ~MixtureParameter() {}
 
     ublas::vector<double> value(const ublas::vector<double>& x) const
     {
@@ -43,9 +43,14 @@ public:
         m_w1 = w1;
     }
     
-    void setStep(double step) { m_step = step; }
+    void setStep(ublas::matrix<double>& step)
+    {
+        m_step = step;
+    }
 
     void setTemperature(double temperature) { m_temperature = temperature; }
+
+    void setRng(RandomGenerator& rng) { m_pRng = &rng; }
 
     double getTemperature() { return m_temperature; }
 
@@ -62,18 +67,15 @@ public:
     
     double prior()
     {
-        double norm2 = 0.0;
+        double rangeout = 0.0;
 
-        ublas::matrix<double> w1 = m_w1;
+        if (m_w1(0, 0) < 0.0)
+            rangeout = 0.0 - m_w1(0, 0);
+                                  
+        if (m_w1(0, 0) > 1.0)
+            rangeout = m_w1(0, 0) - 1.0;
 
-        for (unsigned i = 0; i < w1.size1(); ++i)
-        {
-            ublas::matrix_row< ublas::matrix<double> > mr(w1, i);
-
-            norm2 += inner_prod(mr, mr);
-        }
-                
-        return exp(-norm2 / (2 * 100));
+        return (m_w1(0, 1) * m_w1(0, 1) + m_w1(1, 1) * m_w1(1, 1)) / 50.0 + rangeout * rangeout * 10000.0;
     }
 
     double energy(std::vector<Data>& dataSet, double& h)
@@ -86,6 +88,14 @@ public:
 
             for (unsigned j = 0; j < m_w1.size1(); ++j)
             {
+//                 printf("0 alpha = %f\n", m_w1(0, 0));
+//                 printf("0 mu = %f\n", m_w1(0, 1));
+//                 printf("0 sigma = %f\n", m_w1(0, 2));
+
+//                 printf("1 alpha = %f\n", m_w1(1, 0));
+//                 printf("1 mu = %f\n", m_w1(1, 1));
+//                 printf("1 sigma = %f\n", m_w1(1, 2));
+
                 ublas::vector<double> mu(1);
                 mu(0) = m_w1(j, 1);
 
@@ -93,19 +103,23 @@ public:
                 sum2 += m_w1(j, 0) * exp(- inner_prod(r, r) / 2.0);
             }
 
-            sum1 += log(sum2 / sqrt(2 * 3.14));
+            sum1 += -log(sum2 / sqrt(2 * 3.14));
+
+//            printf("sum1 = %f\n", sum1);
+//          printf("sum2 = %f\n", sum2);
         }
 
         h = sum1;
 
-        return -log(prior()) + m_temperature * h;
+//        printf("e = %f\n", prior() + m_temperature * h);
+        return prior() + m_temperature * h;
     }
 
     void print(FILE* fp)
     {
         for (unsigned i = 0; i < m_w1.size1(); ++i)
             for (unsigned j = 0; j < m_w1.size2(); ++j)
-                fprintf(fp, "%f, ", m_w1(i, j));
+                fprintf(fp, "%f  ", m_w1(i, j));
 
         fprintf(fp, "\n");
     }
@@ -114,9 +128,19 @@ public:
     {
         m_tmp_w1 = m_w1;
 
-        double tmp_step = m_step;
+        if ((i % 3) == 0)
+        {
+            m_w1(0, 0) += (m_pRng->uniform() * 2.0 - 1.0) * m_step(0, 0);
+            m_w1(1, 0) = 1.0 - m_w1(0, 0);
+        }
 
-        m_w1 = m_pRng->uniform2(m_w1, tmp_step * 2.0, -tmp_step, i);
+        if ((i % 3) == 1)
+            m_w1(0, 1) += (m_pRng->uniform() * 2.0 - 1.0) * m_step(0, 1);
+
+        if ((i % 3) == 2)
+            m_w1(1, 1) += (m_pRng->uniform() * 2.0 - 1.0) * m_step(1, 1);
+
+//        print(stdout);
     }
 
     void accept()
@@ -131,7 +155,7 @@ public:
 
     void push(std::vector<IMCMCParameter*>& paramSet)
     {
-        GaussianParameter* p = new GaussianParameter(*m_pRng);
+        MixtureParameter* p = new MixtureParameter();
         *p = *this;
 
         paramSet.push_back(p);
@@ -139,7 +163,7 @@ public:
 
     void swap(IMCMCParameter* pParam)
     {
-        GaussianParameter* pDst = dynamic_cast<GaussianParameter*>(pParam);
+        MixtureParameter* pDst = dynamic_cast<MixtureParameter*>(pParam);
 
         ublas::matrix<double> tmp1 = m_w1;
         
@@ -153,11 +177,11 @@ public:
 
     ublas::matrix<double> m_w1;
 
-    double m_step;
+    ublas::matrix<double> m_step;
     double m_temperature;
 
     ublas::matrix<double> m_tmp_w1;
 };
 
 
-#endif // GAUSSIAN_PARAMETER_H_
+#endif // MIXTURE_PARAMETER_H_
