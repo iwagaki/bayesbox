043b212cf6b7ee195ffe031b538d69ec0e7b95d9
iwagaki <iwagaki@3d40163a-cb53-0410-a544-1fd9c11f5b11>
2008-08-27 01:10:36 +0000

    OptionParser class was re-designed as google-gflags style

--- include/common/OptionParser.h
+++ include/common/OptionParser.h
@@ -2,118 +2,183 @@
 #define OPTION_PARSER_H_
 
 #include <cstdlib>
+#include <vector>
 #include "common/verify.h"
+
+class OptionHolder
+{
+public:
+    OptionHolder(const char* pIdentifier, const char* pDescription) :
+        m_pIdentifier(pIdentifier), m_pDescription(pDescription) {}
+
+    virtual void help() = 0;
+    virtual void setValue(const char* pString) = 0;
+
+    const char* m_pIdentifier;
+    const char* m_pDescription;
+};
+
+
+class OptionHolderInt : public OptionHolder
+{
+public:
+    OptionHolderInt(int value, const char* pIdentifier, const char* pDescription) :
+        OptionHolder(pIdentifier, pDescription), m_value(value) {}
+
+    void help()
+    {
+        printf("    --%-30s  %s (%d)\n", m_pIdentifier, m_pDescription, m_value);
+    }
+
+    void setValue(const char* pString)
+    {
+        VERIFY(pString != 0);
+        m_value = atoi(pString);
+    }
+    
+    int m_value;
+};
+
+
+class OptionHolderDouble : public OptionHolder
+{
+public:
+    OptionHolderDouble(double value, const char* pIdentifier, const char* pDescription) :
+        OptionHolder(pIdentifier, pDescription), m_value(value) {}
+
+    void help()
+    {
+        printf("    --%-30s  %s (%f)\n", m_pIdentifier, m_pDescription, m_value);
+    }
+
+    void setValue(const char* pString)
+    {
+        VERIFY(pString != 0);
+        m_value = atof(pString);
+    }
+    
+    double m_value;
+};
+
+
+class OptionHolderString : public OptionHolder
+{
+public:
+    OptionHolderString(const char* value, const char* pIdentifier, const char* pDescription) :
+        OptionHolder(pIdentifier, pDescription), m_value(value) {}
+    
+    void help()
+    {
+        printf("    --%-30s  %s (%s)\n", m_pIdentifier, m_pDescription, m_value);
+    }
+
+    void setValue(const char* pString)
+    {
+        VERIFY(pString != 0);
+        m_value = pString;
+    }
+    
+    const char* m_value;
+};
+
 
 class OptionParser
 {
 public:
-    OptionParser(int argc, char** argv) : m_argc(argc), m_argv(argv) {}
-    
-    int getInt(char ch, int defaultValue)
+    static int& create(int value, const char* pIdentifier, const char* pDescription)
     {
-        char* pParameter;
-        if (parseParam(ch, pParameter))
-        {
-            if (!pParameter)
-                VERIFY("invalid parameter" && 0);
-            
-            return atoi(pParameter);
-        }
-        
-        return defaultValue;
-    }
- 
-    double getFloat(char ch, double defaultValue)
-    {
-        char* pParameter;
-        if (parseParam(ch, pParameter))
-        {
-            if (!pParameter)
-                VERIFY("invalid parameter" && 0);
-            
-            return atof(pParameter);
-        }
-        
-        return defaultValue;
+        OptionHolderInt* p = new OptionHolderInt(value, pIdentifier, pDescription);
+        m_optionList.push_back(p);
+
+        return p->m_value;
     }
 
-    const char* getString(char ch, const char* defaultValue)
+    static double& create(double value, const char* pIdentifier, const char* pDescription)
     {
-        char* pParameter;
-        if (parseParam(ch, pParameter))
-        {
-            if (!pParameter)
-                VERIFY("invalid parameter" && 0);
-            
-            return pParameter;
-        }
-        
-        return defaultValue;
+        OptionHolderDouble* p = new OptionHolderDouble(value, pIdentifier, pDescription);
+        m_optionList.push_back(p);
+
+        return p->m_value;
     }
 
-    bool isFlag(char ch)
+    static const char*& create(const char* value, const char* pIdentifier, const char* pDescription)
     {
-        char* pParameter;
-        bool isFlag = parseParam(ch, pParameter);
+        OptionHolderString* p = new OptionHolderString(value, pIdentifier, pDescription);
+        m_optionList.push_back(p);
 
-        if (isFlag && pParameter)
-            VERIFY("invalid parameter" && 0);
-
-        return isFlag;
+        return p->m_value;
     }
 
-    bool parseParam(char ch, char*& pParameter)
+    static void helpAll()
+    {
+        printf("OptionHolders:\n");
+
+        for (std::vector<OptionHolder*>::iterator i = m_optionList.begin(); i != m_optionList.end(); ++i)
+            (*i)->help();
+    }
+
+    static void parse(int argc, char** argv)
     {
         int index = 1;
 
-        while (index < m_argc)
+        while (index < argc)
         {
-            if (parse1Param(index, pParameter) == ch)
-                return true;
-        }
+            bool isFound = false;
 
-        return false;
+            char* pParameter;
+            const char* pIdentifier = parse1Param(index, pParameter, argc, argv);
+
+            for (std::vector<OptionHolder*>::iterator i = m_optionList.begin(); i != m_optionList.end(); ++i)
+            {
+                if (strcmp(pIdentifier, (*i)->m_pIdentifier) == 0)
+                {
+                    (*i)->setValue(pParameter);
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (!isFound)
+            {
+                helpAll();
+                exit(-1);
+            }
+        }
     }
 
-    char parse1Param(int& index, char*& pParameter)
+    static const char* parse1Param(int& index, char*& pParameter, int argc, char** argv)
     {
-        if (index >= m_argc)
+        if (index >= argc)
             VERIFY(0);
 
-        char* p = m_argv[index];
-        char ch;
+        char* p = argv[index];
 
         if (*p++ != '-')
             VERIFY("invalid parameter" && 0);
-
-        ch = *p++;
-
-        if (ch < 'a' && ch > 'Z')
-            VERIFY("invalid parameter" && 0);
-
-        if (*p != 0)
+ 
+        if (*p++ != '-')
             VERIFY("invalid parameter" && 0);
 
         ++index;
         pParameter = 0;
 
-        if (index < m_argc)
+        if (index < argc)
         {
-            p = m_argv[index];
+            char* p2 = argv[index];
         
-            if (*p != '-')
+            if (*p2 != '-')
             {
-                pParameter = p;
+                pParameter = p2;
                 ++index;
             }
         }
-
-        return ch;
+        
+        return p;
     }
 
 private:
-    int m_argc;
-    char** m_argv;
+    static std::vector<OptionHolder*> m_optionList;
 };
+
 
 #endif // OPTION_PARSER_H_
