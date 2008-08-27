93949e14bb076f73cca641a3fcae62525deaab01
iwagaki <iwagaki@3d40163a-cb53-0410-a544-1fd9c11f5b11>
2008-08-27 01:15:59 +0000

    fixed a wrong header guard

--- trunk/include/common/OptionParserMain.h
+++ trunk/include/common/OptionParserMain.h
@@ -1,9 +1,9 @@
-#ifndef DEBUG_OPTION_MAIN_H_
-#define DEBUG_OPTION_MAIN_H_
+#ifndef OPTION_PARSER_MAIN_H_
+#define OPTION_PARSER_MAIN_H_
 
 #include <vector>
 #include "common/OptionParser.h"
 
 std::vector<OptionHolder*> OptionParser::m_optionList;
 
-#endif // DEBUG_OPTION_MAIN_H_
+#endif // OPTION_PARSER_MAIN_H_
